f7c79bb5992879ed4157536c0af5e71ee5083fe2
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2022-09-26 19:11:49 +0300

    Start 19

--- Paste_19/Program.cs
+++ Paste_19/Program.cs
@@ -4,3 +4,11 @@
 12821 -> да
 23432 -> да
 */
+
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine());
+if ((number/100==number%10*10+number%100/10)) 
+Console.Write("ДА!");
+else
+    Console.Write("НЕТ");
+
