c7e9aae8db8a783bc5892d1d30a6cecc41cc917f
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2022-09-28 22:04:44 +0300

    Task

---
+++ Task_21/Program.cs
@@ -0,0 +1,79 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+
+Console.WriteLine("Введите координаты точки А: \n");
+int А = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B: \n");
+int B = int.Parse(Console.ReadLine());
+int[] A = {int A}       
+
+
+int x1 = printf(A%10);
+int x2;
+int y1;
+int y2;
+int z1;
+int z2;
+
+
+
+/*
+float  h, c = 0;
+    float a[3], g[3];
+    int i, v, f;
+    printf("Введите координаты по оси x , y, z для точки А  :\n");
+    for(i = 0; i < 3; i++)
+    {
+        scanf("%f", &a[i]);
+    }
+ 
+    printf("Введите координаты по оси x , y, z для точки Б  :\n");
+    for(v = 0; v < 3; v++)
+    {
+        scanf("%f", &g[v]);
+    }
+    for(f = 0; f < 3; f++)
+    {
+    c = c + (a[i] - g[v]) * (a[i] - g[v]);
+    }
+    h = sqrt(c) ;
+    printf("\nрасстояние = %f\n", h);
+    getchar();
+    getchar();
+
+
+    int coordinate;
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+    int z1;
+    int z2;
+ 
+
+
+
+
+    printf("Enter x1 = ");
+    scanf("%x1",&x1);
+    printf("Enter x2 = ");
+    scanf("%x2",&x2);
+    printf("Enter y1 = ");
+    scanf("%y1",&y1);
+    printf("Enter y2 = ");
+    scanf("%y2",&y2);
+    printf("Enter z1 = ");
+    scanf("%z1",&z1);
+    printf("Enter z2 = ");
+    scanf("%z2",&z2);
+ 
+    d=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(x1-x2)+(z1-z2)*(z1-z2));
+ 
+    printf("Distance between points = %d", d);
+    printf("\n");
+}
+*/
