508b54c2d4fef4447c7fb3165eb7726f86392b40
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2022-10-01 17:24:56 +0300

    Task 23

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -6,6 +6,6 @@
 Console.WriteLine("Введите число:");
 int number = int.Parse(Console.ReadLine());
 {
-    for (int i = 0; i < number; i++)
+    for (int i = 1; i < number; i++)
         Console.WriteLine(i * i * i);
 }
